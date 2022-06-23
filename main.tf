b3db072cc6a657fd3c6ecbe09aa298ded5a1ed2a
Santhosh Kumar <santhoshmail2u@gmail.com>
2022-06-23 07:16:04 +0530

    update main file

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
+resource "azurerm_resource_group" "devops" {
+  name     = "devops"
+  location = "East US 2"
+}
+
 resource "azurerm_resource_group" "webapp" {
   name     = "webapp"
   location = "East US 2"
 }
-
-# resource "azurerm_resource_group" "webapp" {
-#   name     = "webapp"
-#   location = "East US 2"
-# }
